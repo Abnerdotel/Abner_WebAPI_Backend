82b1cbdb4931e45d6eff6830acc247e26f7d15d8
Abner David Dotel Chalas <130618456+Abnerdotel@users.noreply.github.com>
2024-07-13 00:46:36 -0400

    Update ContactController.cs

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -9,31 +9,6 @@ namespace Abner_WebAPI_Backend.Controllers
     public class ContactController : ControllerBase
     {
 
-        //Parte a trabajar:
-
-        /**Desarrollar la logica para cada elemento del contacto el cual estara 
-         vinculado con el usuario (cada use tendra su porpio contact)
-        que debera hacerce cada operacion vista debajo
-        **/
-
-        //Get contact
-        //[HttpGet("{contactid}")]
-        // public IActionResult GetContacts(int ContactId) { }
-
-
-        //Add contact
-        //[HttpPost]
-        //public IActionResult AddContact([FromBody] ContactModel contact) { }
-
-        //Edit contact
-        // [HttpPut("{contactid}")]
-        //public IActionResult EditContact(int contactId, [FromBody] ContactModel contact) { }
-
-
-
-        //Delete contact
-        //[HttpDelete("{contactid}")]
-        // public IActionResult DeleteContact(int contacId) { }
 
 
     }
